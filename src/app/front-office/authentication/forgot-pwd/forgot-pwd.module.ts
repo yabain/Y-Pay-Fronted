5581f626baa011b36ff30676ef1f19aaaec29a64
Flambel SANOU <flambel55@gmail.com>
2022-10-18 20:31:06 +0100

    update forgot-pwd build prod error

--- src/app/front-office/authentication/forgot-pwd/forgot-pwd.module.ts
+++ src/app/front-office/authentication/forgot-pwd/forgot-pwd.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ForgotPwdComponent } from './Forgot-pwd.component';
-import { ForgotPwdRoutingModule } from './Forgot-pwd-routing.module';
+import { ForgotPwdComponent } from './forgot-pwd.component';
+import { ForgotPwdRoutingModule } from './forgot-pwd-routing.module';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
