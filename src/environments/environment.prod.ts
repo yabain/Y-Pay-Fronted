239ceffb65bcdfb7395eb01fef39d829471a4efe
Cedric-Yaba-In <c.nguendap@yabain.com>
2022-10-19 04:42:13 +0100

    change url backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  url:  'http://api-dev.y-nkap.com',
+  // url:  'http://api-dev.y-nkap.com',
+  url:  'http://ynkap-dev-env.eba-qgbsbenk.eu-central-1.elasticbeanstalk.com',
   urlProd:  'http://api.y-nkap.com'
 };
